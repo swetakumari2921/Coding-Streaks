407dc8351fa208107432d16b9567d8e6e98cd98f
swetakumari2921 <swetaamouna@gmail.com>
2026-02-19 20:58:44 +0530

    Day-50

---
+++ CodingStreaks/src/my/collection/linkedlist/practice/Ex46.java
@@ -0,0 +1,31 @@
+package my.collection.linkedlist.practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex46 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number of friends:");
+		int n = sc.nextInt();
+
+		ArrayList<Integer> scores = new ArrayList<>();
+		System.out.println("Enter the scores of each friend:");
+		for (int i = 0; i < n; i++) {
+			scores.add(sc.nextInt());
+		}
+
+		int maxScore = -1;
+		int winnerIndex = -1;
+		for (int i = 0; i < n; i++) {
+			if (scores.get(i) > maxScore) {
+				maxScore = scores.get(i);
+				winnerIndex = i;
+			}
+		}
+
+		System.out.println("The winning friend index (0-based) is: " + winnerIndex);
+	}
+}
