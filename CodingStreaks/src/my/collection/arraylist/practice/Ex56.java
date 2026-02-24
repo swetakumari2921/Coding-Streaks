5cc28cab93f5b4cf26d4aa213c8398dbf25f4c5b
swetakumari2921 <swetaamouna@gmail.com>
2026-02-24 21:13:01 +0530

    Day-55

---
+++ CodingStreaks/src/my/collection/arraylist/practice/Ex56.java
@@ -0,0 +1,27 @@
+package my.collection.arraylist.practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex56 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number of fruits:");
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		ArrayList<String> list = new ArrayList<>();
+
+		System.out.println("Enter fruit names:");
+		for (int i = 0; i < n; i++) {
+			String fruit = sc.nextLine();
+			list.add(fruit);
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+}
