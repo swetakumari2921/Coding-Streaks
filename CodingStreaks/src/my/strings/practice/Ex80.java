2cc6f5069a2a2771f93793d018c93656985e2bf0
swetakumari2921 <swetaamouna@gmail.com>
2026-03-05 17:52:55 +0530

    Day-64

---
+++ CodingStreaks/src/my/strings/practice/Ex80.java
@@ -0,0 +1,68 @@
+package my.strings.practice;
+
+import java.util.Scanner;
+
+public class Ex80 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter first string:");
+		String s1 = sc.nextLine();
+
+		System.out.println("Enter second string:");
+		String s2 = sc.nextLine();
+
+		int m = s1.length();
+		int n = s2.length();
+
+		int[][] dp = new int[m + 1][n + 1];
+
+		for (int i = 0; i <= m; i++) {
+			for (int j = 0; j <= n; j++) {
+
+				if (i == 0)
+					dp[i][j] = j;
+				else if (j == 0)
+					dp[i][j] = i;
+				else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				else
+					dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+
+		int i = m;
+		int j = n;
+
+		String result = "";
+
+		while (i > 0 && j > 0) {
+
+			if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+				result = s1.charAt(i - 1) + result;
+				i--;
+				j--;
+			} else if (dp[i - 1][j] < dp[i][j - 1]) {
+				result = s1.charAt(i - 1) + result;
+				i--;
+			} else {
+				result = s2.charAt(j - 1) + result;
+				j--;
+			}
+		}
+
+		while (i > 0) {
+			result = s1.charAt(i - 1) + result;
+			i--;
+		}
+
+		while (j > 0) {
+			result = s2.charAt(j - 1) + result;
+			j--;
+		}
+
+		System.out.println("Shortest Common Supersequence: " + result);
+	}
+}
