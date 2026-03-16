eec0587b35ddb7e892499b57df3e02e3a611542c
swetakumari2921 <swetaamouna@gmail.com>
2026-03-16 19:32:14 +0530

    Day-75

---
+++ CodingStreaks/src/my/strings/practice/Ex91.java
@@ -0,0 +1,42 @@
+package my.strings.practice;
+
+public class Ex91 {
+	public static boolean isMatch(String s, String p) {
+		int m = s.length();
+		int n = p.length();
+
+		boolean[][] dp = new boolean[m + 1][n + 1];
+		dp[0][0] = true;
+
+		for (int j = 1; j <= n; j++) {
+			if (p.charAt(j - 1) == '*')
+				dp[0][j] = dp[0][j - 1];
+		}
+
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+
+				if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1))
+					dp[i][j] = dp[i - 1][j - 1];
+
+				else if (p.charAt(j - 1) == '*')
+					dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+				else
+					dp[i][j] = false;
+			}
+		}
+
+		return dp[m][n];
+	}
+
+	public static void main(String[] args) {
+		String str = "abcd";
+		String pattern = "a*d";
+
+		if (isMatch(str, pattern))
+			System.out.println("Match");
+		else
+			System.out.println("Not Match");
+	}
+}
