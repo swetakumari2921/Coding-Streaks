147c7a3630d848e85fa601f288a6fe661de1fb86
swetakumari2921 <swetaamouna@gmail.com>
2026-03-23 19:03:45 +0530

    Day-82

---
+++ CodingStreaks/src/my/arrays/practice/Ex98.java
@@ -0,0 +1,42 @@
+package my.arrays.practice;
+
+public class Ex98 {
+	public static void main(String[] args) {
+		int arr[] = { 1, 0, 1, 0, 1 };
+
+		int n = arr.length;
+		int totalOnes = 0;
+
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == 1)
+				totalOnes++;
+		}
+
+		if (totalOnes == 0) {
+			System.out.println(0);
+			return;
+		}
+
+		int currOnes = 0;
+
+		for (int i = 0; i < totalOnes; i++) {
+			if (arr[i] == 1)
+				currOnes++;
+		}
+
+		int maxOnes = currOnes;
+
+		for (int i = totalOnes; i < n; i++) {
+			if (arr[i] == 1)
+				currOnes++;
+			if (arr[i - totalOnes] == 1)
+				currOnes--;
+
+			if (currOnes > maxOnes)
+				maxOnes = currOnes;
+		}
+
+		int minSwaps = totalOnes - maxOnes;
+		System.out.println("Minimum swaps: " + minSwaps);
+	}
+}
