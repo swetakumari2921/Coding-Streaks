35e59c8a6d57fe6d1d4940dd93170fd09598a66f
swetakumari2921 <swetaamouna@gmail.com>
2026-03-25 12:36:28 +0530

    Day-84

---
+++ CodingStreaks/src/my/arrays/practice/Ex101.java
@@ -0,0 +1,14 @@
+package my.arrays.practice;
+
+public class Ex101 {
+	public static void main(String[] args) {
+		int arr[] = { 2, 3, 4, 5 };
+		int product = 1;
+
+		for (int i = 0; i < arr.length; i++) {
+			product = product * arr[i];
+		}
+
+		System.out.println(product);
+	}
+}
