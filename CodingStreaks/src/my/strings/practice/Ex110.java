e893a92a65a916bcd9be8cdac2078a8cd7ef3e59
swetakumari2921 <swetaamouna@gmail.com>
2026-04-03 22:35:39 +0530

    Day-93

--- CodingStreaks/src/my/strings/practice/Ex110.java
+++ CodingStreaks/src/my/strings/practice/Ex110.java
@@ -2,12 +2,64 @@ package my.strings.practice;
 
 public class Ex110 {
 	public static void main(String[] args) {
-		int[] arr = { 10, 20, 30, 40, 50, 60 };
+		String str = "mamad";
+		char[] arr = str.toCharArray();
+		int n = arr.length;
+		int swaps = 0;
 
-		for (int i = 0; i < arr.length; i++) {
-			if (i % 2 == 0) {
-				System.out.print(arr[i] + " ");
+		if (!canFormPalindrome(arr)) {
+			System.out.println(-1);
+			return;
+		}
+
+		int i = 0, j = n - 1;
+
+		while (i < j) {
+			if (arr[i] == arr[j]) {
+				i++;
+				j--;
+			} else {
+				int k = j;
+				while (k > i && arr[k] != arr[i]) {
+					k--;
+				}
+
+				if (k == i) {
+					char temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					swaps++;
+				} else {
+					while (k < j) {
+						char temp = arr[k];
+						arr[k] = arr[k + 1];
+						arr[k + 1] = temp;
+						swaps++;
+						k++;
+					}
+					i++;
+					j--;
+				}
 			}
 		}
+
+		System.out.println(swaps);
+	}
+
+	static boolean canFormPalindrome(char[] arr) {
+		int[] freq = new int[256];
+
+		for (int i = 0; i < arr.length; i++) {
+			freq[arr[i]]++;
+		}
+
+		int odd = 0;
+		for (int i = 0; i < 256; i++) {
+			if (freq[i] % 2 != 0) {
+				odd++;
+			}
+		}
+
+		return odd <= 1;
 	}
 }
