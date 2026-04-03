e7df9105158b74d42de263d5ece97c30772e44ef
swetakumari2921 <swetaamouna@gmail.com>
2026-04-03 22:32:02 +0530

    Day-93

---
+++ CodingStreaks/src/my/arrays/practice/Ex110.java
@@ -0,0 +1,13 @@
+package my.arrays.practice;
+
+public class Ex110 {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 40, 50, 60 };
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
