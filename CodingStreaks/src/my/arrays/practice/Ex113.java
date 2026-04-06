da49c9801c5e1ad5b67ac968598b65d340915cbc
swetakumari2921 <swetaamouna@gmail.com>
2026-04-06 21:30:27 +0530

    Day-96

---
+++ CodingStreaks/src/my/arrays/practice/Ex113.java
@@ -0,0 +1,16 @@
+package my.arrays.practice;
+
+public class Ex113 {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+		for (int i = 0; i < arr.length - 1; i += 2) {
+			int temp = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = temp;
+		}
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
+	}
+}
