242cf250ec3246197d239eba6f5ea93e7d8882c9
swetakumari2921 <swetaamouna@gmail.com>
2026-04-09 21:52:30 +0530

    Day-99

---
+++ CodingStreaks/src/my/arrays/practice/Ex116.java
@@ -0,0 +1,19 @@
+package my.arrays.practice;
+
+public class Ex116 {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+
+		int last = arr[arr.length - 1];
+
+		for (int i = arr.length - 1; i > 0; i--) {
+			arr[i] = arr[i - 1];
+		}
+
+		arr[0] = last;
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
