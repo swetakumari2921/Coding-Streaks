a341e9f4b6bc2fe6ef2e7b1cd73592c92c8ba1ce
swetakumari2921 <swetaamouna@gmail.com>
2026-04-09 22:00:01 +0530

    Day-99

---
+++ CodingStreaks/src/my/collection/linkedlist/practice/Ex97.java
@@ -0,0 +1,13 @@
+package my.collection.linkedlist.practice;
+
+public class Ex97 {
+	public static void main(String[] args) {
+		String str = "hello";
+
+		if (str.length() % 2 == 0) {
+			System.out.println("Even length");
+		} else {
+			System.out.println("Odd length");
+		}
+	}
+}
