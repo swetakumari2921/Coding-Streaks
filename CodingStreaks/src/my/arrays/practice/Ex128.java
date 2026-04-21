5bdd6c8c63fa7f1a8d67f40306f84088f9a03600
swetakumari2921 <swetaamouna@gmail.com>
2026-04-21 20:31:01 +0530

    Day-111

---
+++ CodingStreaks/src/my/arrays/practice/Ex128.java
@@ -0,0 +1,11 @@
+package my.arrays.practice;
+
+public class Ex128 {
+	public static void main(String[] args) {
+		double num = 5.3;
+
+		int ceilValue = (int) Math.ceil(num);
+
+		System.out.println(ceilValue);
+	}
+}
