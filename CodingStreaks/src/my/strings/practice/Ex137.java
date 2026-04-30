01acf97a2621c8a6865a3a2b6bd5a71349007f9a
swetakumari2921 <swetaamouna@gmail.com>
2026-04-30 22:03:21 +0530

    Day-120

--- CodingStreaks/src/my/strings/practice/Ex137.java
+++ CodingStreaks/src/my/strings/practice/Ex137.java
@@ -1,13 +1,12 @@
 package my.strings.practice;
 
 public class Ex137 {
+	public static boolean exist(char[][] board, String word) {
+		int n = board.length;
+		int m = board[0].length;
 
-	static boolean exist(char[][] board, String word) {
-		int rows = board.length;
-		int cols = board[0].length;
-
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				if (dfs(board, word, i, j, 0)) {
 					return true;
 				}
@@ -16,22 +15,22 @@ public class Ex137 {
 		return false;
 	}
 
-	static boolean dfs(char[][] board, String word, int i, int j, int index) {
+	static boolean dfs(char[][] board, String word, int i, int j, int idx) {
 
-		if (index == word.length())
+		if (idx == word.length())
 			return true;
 
 		if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)
 			return false;
 
-		if (board[i][j] != word.charAt(index))
+		if (board[i][j] != word.charAt(idx))
 			return false;
 
 		char temp = board[i][j];
 		board[i][j] = '#'; // mark visited
 
-		boolean found = dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1)
-				|| dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1);
+		boolean found = dfs(board, word, i + 1, j, idx + 1) || dfs(board, word, i - 1, j, idx + 1)
+				|| dfs(board, word, i, j + 1, idx + 1) || dfs(board, word, i, j - 1, idx + 1);
 
 		board[i][j] = temp; // backtrack
 
@@ -41,7 +40,7 @@ public class Ex137 {
 	public static void main(String[] args) {
 		char[][] board = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
 
-		String word = "ABCCED";
+		String word = "SEE";
 
 		System.out.println(exist(board, word));
 	}
