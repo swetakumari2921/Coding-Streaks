6e7d0b2b4f7f6d30f16748a2b3af62ad9edd029d
swetakumari2921 <swetaamouna@gmail.com>
2026-05-05 18:47:02 +0530

    Day-125

---
+++ CodingStreaks/src/my/arrays/practice/Ex142.java
@@ -0,0 +1,29 @@
+package my.arrays.practice;
+
+public class Ex142 {
+	public static int removeDuplicates(int[] arr, int n) {
+		if (n == 0)
+			return 0;
+
+		int j = 0; // index for unique elements
+
+		for (int i = 1; i < n; i++) {
+			if (arr[i] != arr[j]) {
+				j++;
+				arr[j] = arr[i];
+			}
+		}
+		return j + 1; // new size
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 1, 2, 2, 3, 4, 4 };
+		int n = arr.length;
+
+		int newSize = removeDuplicates(arr, n);
+
+		for (int i = 0; i < newSize; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
