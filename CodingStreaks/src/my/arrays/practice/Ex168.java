109463074ae481c2af7c49ad8d4d3f83ff7fc93d
swetakumari2921 <swetaamouna@gmail.com>
2026-05-25 20:27:58 +0530

    Day-145

---
+++ CodingStreaks/src/my/arrays/practice/Ex168.java
@@ -0,0 +1,43 @@
+package my.arrays.practice;
+
+public class Ex168 {
+	// function to count digits
+	static int countDigits(int n) {
+		n = Math.abs(n); // handle negative numbers
+		int count = 0;
+
+		if (n == 0)
+			return 1;
+
+		while (n > 0) {
+			count++;
+			n = n / 10;
+		}
+		return count;
+	}
+
+	// function to find number with max digits
+	static int findMaxDigitNumber(int[] arr) {
+		int maxDigits = 0;
+		int result = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			int digits = countDigits(arr[i]);
+
+			if (digits > maxDigits) {
+				maxDigits = digits;
+				result = arr[i];
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 12, 345, 6, 7890, 45 };
+
+		int ans = findMaxDigitNumber(arr);
+
+		System.out.println("Number with maximum digits: " + ans);
+	}
+}
