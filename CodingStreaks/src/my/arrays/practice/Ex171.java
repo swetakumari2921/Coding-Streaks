f7884ddff0a32729cf2bf3808323349d52320b26
swetakumari2921 <swetaamouna@gmail.com>
2026-05-28 15:31:03 +0530

    Day-148

---
+++ CodingStreaks/src/my/arrays/practice/Ex171.java
@@ -0,0 +1,44 @@
+package my.arrays.practice;
+
+import java.util.Scanner;
+
+public class Ex171 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter size of array:");
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		System.out.println("Enter array elements:");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int maxFreq = 0;
+		int element = arr[0];
+
+		for (int i = 0; i < n; i++) {
+
+			int count = 0;
+
+			for (int j = 0; j < n; j++) {
+
+				if (arr[i] == arr[j]) {
+					count++;
+				}
+			}
+
+			if (count > maxFreq) {
+				maxFreq = count;
+				element = arr[i];
+			}
+		}
+
+		System.out.println("Element with highest frequency: " + element);
+		System.out.println("Frequency: " + maxFreq);
+	}
+}
