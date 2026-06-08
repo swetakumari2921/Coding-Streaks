5fc993a61f6945d0a2954dd3fec2077c030d4eac
swetakumari2921 <swetaamouna@gmail.com>
2026-06-08 18:59:45 +0530

    Day-158

---
+++ CodingStreaks/src/my/strings/practice/Ex179.java
@@ -0,0 +1,39 @@
+package my.strings.practice;
+
+public class Ex179 {
+
+	static boolean isPalindrome(String s, int left, int right) {
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+
+	static boolean canBecomePalindrome(String s) {
+		int left = 0;
+		int right = s.length() - 1;
+
+		while (left < right) {
+			if (s.charAt(left) != s.charAt(right)) {
+				return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+			}
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String s = "abca";
+
+		if (canBecomePalindrome(s))
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
