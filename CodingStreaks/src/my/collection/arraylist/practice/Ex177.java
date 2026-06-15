725451745a8fea22d7adee5da0da8e6add4315e3
swetakumari2921 <swetaamouna@gmail.com>
2026-06-15 09:54:04 +0530

    Day-166

---
+++ CodingStreaks/src/my/collection/arraylist/practice/Ex177.java
@@ -0,0 +1,40 @@
+package my.collection.arraylist.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ex177 {
+
+	public class Ex96 {
+
+	    public static ArrayList<Integer> replaceWithRank(int[] arr) {
+
+	        ArrayList<Integer> sorted = new ArrayList<>();
+
+	        for (int num : arr) {
+	            if (!sorted.contains(num)) {
+	                sorted.add(num);
+	            }
+	        }
+
+	        Collections.sort(sorted);
+
+	        ArrayList<Integer> ranks = new ArrayList<>();
+
+	        for (int num : arr) {
+	            int rank = sorted.indexOf(num) + 1;
+	            ranks.add(rank);
+	        }
+
+	        return ranks;
+	    }
+
+	    public static void main(String[] args) {
+
+	        int[] arr = {40, 10, 20, 30};
+
+	        ArrayList<Integer> result = replaceWithRank(arr);
+
+	        System.out.println("Ranks: " + result);
+	    }
+	}
