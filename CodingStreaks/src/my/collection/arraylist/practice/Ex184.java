6004aecf56937b3d5d9e00ada4378e68df5447bd
swetakumari2921 <swetaamouna@gmail.com>
2026-06-22 21:17:42 +0530

    Day-173

--- CodingStreaks/src/my/collection/arraylist/practice/Ex184.java
+++ CodingStreaks/src/my/collection/arraylist/practice/Ex184.java
@@ -1,52 +1,62 @@
 package my.collection.arraylist.practice;
 
+import java.util.ArrayList;
+
 public class Ex184 {
 
-	static boolean isScramble(String s1, String s2) {
+	static int firstPosition(ArrayList<Integer> list, int target) {
+		int low = 0, high = list.size() - 1;
+		int result = -1;
 
-		if (s1.equals(s2))
-			return true;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
 
-		if (s1.length() != s2.length())
-			return false;
-
-		int n = s1.length();
-
-		// Check character frequency
-		int[] count = new int[256];
-
-		for (int i = 0; i < n; i++) {
-			count[s1.charAt(i)]++;
-			count[s2.charAt(i)]--;
+			if (list.get(mid) == target) {
+				result = mid;
+				high = mid - 1; // search left side
+			} else if (list.get(mid) < target) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
 		}
 
-		for (int i = 0; i < 256; i++) {
-			if (count[i] != 0)
-				return false;
+		return result;
+	}
+
+	static int lastPosition(ArrayList<Integer> list, int target) {
+		int low = 0, high = list.size() - 1;
+		int result = -1;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+
+			if (list.get(mid) == target) {
+				result = mid;
+				low = mid + 1; // search right side
+			} else if (list.get(mid) < target) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
 		}
 
-		for (int i = 1; i < n; i++) {
-
-			// Without swapping
-			if (isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i)))
-				return true;
-
-			// With swapping
-			if (isScramble(s1.substring(0, i), s2.substring(n - i))
-					&& isScramble(s1.substring(i), s2.substring(0, n - i)))
-				return true;
-		}
-
-		return false;
+		return result;
 	}
 
 	public static void main(String[] args) {
-		String s1 = "great";
-		String s2 = "rgeat";
+		ArrayList<Integer> list = new ArrayList<>();
 
-		if (isScramble(s1, s2))
-			System.out.println("Scrambled String");
-		else
-			System.out.println("Not a Scrambled String");
+		list.add(1);
+		list.add(2);
+		list.add(2);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+
+		int target = 2;
+
+		System.out.println("First Position: " + firstPosition(list, target));
+		System.out.println("Last Position: " + lastPosition(list, target));
 	}
 }
