211b017100d679aafea68a1a81e287efbb64f296
swetakumari2921 <swetaamouna@gmail.com>
2026-06-24 19:45:10 +0530

    Day-175

---
+++ CodingStreaks/src/my/strings/practice/Ex195.java
@@ -0,0 +1,32 @@
+package my.strings.practice;
+
+public class Ex195 {
+
+	public static int countPalSubseq(String s) {
+		int n = s.length();
+		int[][] dp = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			dp[i][i] = 1;
+		}
+
+		for (int len = 2; len <= n; len++) {
+			for (int i = 0; i <= n - len; i++) {
+				int j = i + len - 1;
+
+				if (s.charAt(i) == s.charAt(j)) {
+					dp[i][j] = dp[i + 1][j] + dp[i][j - 1] + 1;
+				} else {
+					dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
+				}
+			}
+		}
+
+		return dp[0][n - 1];
+	}
+
+	public static void main(String[] args) {
+		String s = "aaa";
+		System.out.println(countPalSubseq(s));
+	}
+}
