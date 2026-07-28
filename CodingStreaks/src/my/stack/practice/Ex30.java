c3395d77dcac70599a7d71eb09cd763df87c0133
swetakumari2921 <swetaamouna@gmail.com>
2026-07-28 20:42:00 +0530

    Day-209

---
+++ CodingStreaks/src/my/stack/practice/Ex30.java
@@ -0,0 +1,35 @@
+package my.stack.practice;
+
+import java.util.Stack;
+
+public class Ex30 {
+
+	public static int longestValid(String s) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(-1);
+
+		int maxLength = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				stack.push(i);
+			} else {
+				stack.pop();
+
+				if (stack.isEmpty()) {
+					stack.push(i);
+				} else {
+					maxLength = Math.max(maxLength, i - stack.peek());
+				}
+			}
+		}
+
+		return maxLength;
+	}
+
+	public static void main(String[] args) {
+		String s = "(()())())";
+
+		System.out.println("Longest Valid Parentheses Length: " + longestValid(s));
+	}
+}
