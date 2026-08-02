c1275c328f9896202a542da7934ba1a5f33429ac
swetakumari2921 <swetaamouna@gmail.com>
2026-08-02 20:42:34 +0530

    Day-214

--- CodingStreaks/src/my/queue/practice/Ex35.java
+++ CodingStreaks/src/my/queue/practice/Ex35.java
@@ -3,48 +3,46 @@ package my.queue.practice;
 import java.util.LinkedList;
 import java.util.Queue;
 
+class Process {
+	int id;
+	int burstTime;
+
+	Process(int id, int burstTime) {
+		this.id = id;
+		this.burstTime = burstTime;
+	}
+}
+
 public class Ex35 {
 
-	class Process {
-		int id;
-		int burstTime;
+	public static void schedule(Queue<Process> queue) {
 
-		Process(int id, int burstTime) {
-			this.id = id;
-			this.burstTime = burstTime;
+		int currentTime = 0;
+
+		while (!queue.isEmpty()) {
+
+			Process process = queue.poll();
+
+			System.out.println("Executing Process P" + process.id);
+
+			currentTime += process.burstTime;
+
+			System.out.println("Completion Time: " + currentTime);
+			System.out.println();
 		}
 	}
 
-	public class CPUSchedulingSimulation {
+	public static void main(String[] args) {
 
-	    public static void schedule(Queue<Process> queue) {
+		Queue<Process> queue = new LinkedList<>();
 
-	        int currentTime = 0;
+		// Adding processes (FCFS order)
+		queue.add(new Process(1, 5));
+		queue.add(new Process(2, 3));
+		queue.add(new Process(3, 4));
 
-	        while (!queue.isEmpty()) {
+		System.out.println("CPU Scheduling Simulation:");
 
-	            Process process = queue.poll();
-
-	            System.out.println("Executing Process P" + process.id);
-
-	            currentTime += process.burstTime;
-
-	            System.out.println("Completion Time: " + currentTime);
-	            System.out.println();
-	        }
-	    }
-
-	    public static void main(String[] args) {
-
-	        Queue<Process> queue = new LinkedList<>();
-
-	        // Adding processes (FCFS order)
-	        queue.add(new Process(1, 5));
-	        queue.add(new Process(2, 3));
-	        queue.add(new Process(3, 4));
-
-	        System.out.println("CPU Scheduling Simulation:");
-
-	        schedule(queue);
-	    }
+		schedule(queue);
 	}
+}
