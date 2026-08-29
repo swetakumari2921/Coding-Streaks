160af2985d6dc2cc3717a7db1ef5ee771fbaa781
swetakumari2921 <swetaamouna@gmail.com>
2026-08-29 20:49:39 +0530

    Day-01

---
+++ CodingStreaks/src/my09/collection/hashset/practice/Ex01.java
@@ -0,0 +1,19 @@
+package my09.collection.hashset.practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<>();
+
+		set.add(10);
+		set.add(20);
+		set.add(30);
+		set.add(40);
+		set.add(50);
+
+		System.out.println(set);
+	}
+}
