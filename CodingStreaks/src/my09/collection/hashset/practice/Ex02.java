df920d595c4af81d19a1efb7818eda1992dcc03e
swetakumari2921 <swetaamouna@gmail.com>
2026-08-30 18:56:00 +0530

    Day-02

---
+++ CodingStreaks/src/my09/collection/hashset/practice/Ex02.java
@@ -0,0 +1,18 @@
+package my09.collection.hashset.practice;
+
+import java.util.HashSet;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		HashSet<String> names = new HashSet<>();
+		names.add("Rahul");
+		names.add("Rohan");
+		names.add("Amit");
+		names.add("John");
+		names.add("Nakul");
+
+		System.out.println(names);
+	}
+}
