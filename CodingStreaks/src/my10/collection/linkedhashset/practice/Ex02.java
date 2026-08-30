dc14ed8bb9da2b8d2eee180433520467ae2a565e
swetakumari2921 <swetaamouna@gmail.com>
2026-08-30 18:59:23 +0530

    Day-02

---
+++ CodingStreaks/src/my10/collection/linkedhashset/practice/Ex02.java
@@ -0,0 +1,19 @@
+package my10.collection.linkedhashset.practice;
+
+import java.util.LinkedHashSet;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		LinkedHashSet<String> names = new LinkedHashSet<>();
+
+		names.add("Aditya");
+		names.add("Harsh");
+		names.add("Niyat");
+		names.add("Nayak");
+		names.add("Sanket");
+
+		System.out.println(names);
+	}
+}
