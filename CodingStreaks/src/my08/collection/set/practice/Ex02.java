dc42ea5f8763ff9712f339e1e13e01a745ceb528
swetakumari2921 <swetaamouna@gmail.com>
2026-08-30 18:52:51 +0530

    Day-02

---
+++ CodingStreaks/src/my08/collection/set/practice/Ex02.java
@@ -0,0 +1,20 @@
+package my08.collection.set.practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		Set<String> name = new HashSet<>();
+
+		name.add("Sweta");
+		name.add("Shilpa");
+		name.add("Kiran");
+		name.add("Kushal");
+		name.add("Meera");
+
+		System.out.println(name);
+	}
+}
