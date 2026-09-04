99f45821f192dd8841b2a825dae859eee8b68cb2
swetakumari2921 <swetaamouna@gmail.com>
2026-09-04 22:08:53 +0530

    Day-07

--- CodingStreaks/src/my09/collection/hashset/practice/Ex07.java
+++ CodingStreaks/src/my09/collection/hashset/practice/Ex07.java
@@ -1,5 +1,23 @@
 package my09.collection.hashset.practice;
 
-public class Ex07 {
+import java.util.*;
 
+public class Ex07 {
+	public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<>();
+
+        if (numbers.isEmpty()) {
+            System.out.println("HashSet is empty.");
+        } else {
+            System.out.println("HashSet is not empty.");
+        }
+
+        numbers.add(100);
+
+        if (numbers.isEmpty()) {
+            System.out.println("HashSet is empty.");
+        } else {
+            System.out.println("HashSet is not empty: " + numbers);
+        }
+    }
 }
