7e466e5f8dc490d9c676c37bb206717e06fa8f93
swetakumari2921 <swetaamouna@gmail.com>
2026-09-06 19:45:58 +0530

    Day-09

--- CodingStreaks/src/my09/collection/hashset/practice/Ex09.java
+++ CodingStreaks/src/my09/collection/hashset/practice/Ex09.java
@@ -1,5 +1,22 @@
 package my09.collection.hashset.practice;
 
-public class Ex09 {
+import java.util.HashSet;
 
+public class Ex09 {
+	public static void main(String[] args) {
+
+		HashSet<Integer> numbers = new HashSet<>();
+
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		numbers.add(50);
+
+		System.out.println("Before removing: " + numbers);
+
+		numbers.remove(30);
+
+		System.out.println("After removing: " + numbers);
+	}
 }
