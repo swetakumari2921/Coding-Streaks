0271357cea428bbff35afda0de1dee20474fac5e
swetakumari2921 <swetaamouna@gmail.com>
2026-09-07 21:37:47 +0530

    Day-10

---
+++ CodingStreaks/src/my10/collection/linkedhashset/practice/Ex10.java
@@ -0,0 +1,22 @@
+package my10.collection.linkedhashset.practice;
+
+import java.util.LinkedHashSet;
+
+public class Ex10 {
+	public static void main(String[] args) {
+
+		LinkedHashSet<Integer> numbers = new LinkedHashSet<>();
+
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		numbers.add(50);
+
+		System.out.println("Before removing: " + numbers);
+
+		numbers.clear();
+
+		System.out.println("After removing: " + numbers);
+	}
+}
