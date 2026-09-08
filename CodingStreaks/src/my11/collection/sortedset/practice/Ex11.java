930b95ecb4f24047ed7e41f597b87b3104d862d6
swetakumari2921 <swetaamouna@gmail.com>
2026-09-08 07:04:30 +0530

    Day-11

--- CodingStreaks/src/my11/collection/sortedset/practice/Ex11.java
+++ CodingStreaks/src/my11/collection/sortedset/practice/Ex11.java
@@ -1,5 +1,19 @@
 package my11.collection.sortedset.practice;
 
-public class Ex11 {
+import java.util.*;
 
+public class Ex11 {
+	public static void main(String[] args) {
+
+		SortedSet<Integer> numbers = new TreeSet<>();
+
+		numbers.add(30);
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(10);
+		numbers.add(30);
+		numbers.add(20);
+
+		System.out.println(numbers);
+	}
 }
