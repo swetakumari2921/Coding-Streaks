862c73c8eed971bee99fed0c0a8d9bd20c729fa7
swetakumari2921 <swetaamouna@gmail.com>
2026-09-08 06:59:59 +0530

    Day-11

---
+++ CodingStreaks/src/my09/collection/hashset/practice/Ex11.java
@@ -0,0 +1,18 @@
+package my09.collection.hashset.practice;
+
+import java.util.HashSet;
+
+public class Ex11 {
+	public static void main(String[] args) {
+
+		HashSet<Integer> numbers = new HashSet<>();
+
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+
+		System.out.println(numbers);
+	}
+}
