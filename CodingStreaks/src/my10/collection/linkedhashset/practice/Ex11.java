ec4c11b65de5c215f8a1022378cf96b275315ad1
swetakumari2921 <swetaamouna@gmail.com>
2026-09-08 07:01:40 +0530

    Day-11

---
+++ CodingStreaks/src/my10/collection/linkedhashset/practice/Ex11.java
@@ -0,0 +1,18 @@
+package my10.collection.linkedhashset.practice;
+
+import java.util.LinkedHashSet;
+
+public class Ex11 {
+	public static void main(String[] args) {
+
+		LinkedHashSet<Integer> numbers = new LinkedHashSet<>();
+
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+		numbers.add(10);
+
+		System.out.println(numbers);
+	}
+}
