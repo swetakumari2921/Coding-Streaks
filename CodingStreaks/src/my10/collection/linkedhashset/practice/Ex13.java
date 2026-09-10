d6d585206e24faaa7b9bac1703acabd03655216a
swetakumari2921 <swetaamouna@gmail.com>
2026-09-10 22:03:09 +0530

    Day-13

---
+++ CodingStreaks/src/my10/collection/linkedhashset/practice/Ex13.java
@@ -0,0 +1,19 @@
+package my10.collection.linkedhashset.practice;
+
+import java.util.*;
+
+public class Ex13 {
+	public static void main(String[] args) {
+
+		Set<Integer> set = new HashSet<>();
+
+		set.add(null);
+		set.add(null);
+		set.add(null);
+		set.add(null);
+		set.add(null);
+
+		System.out.println(set);
+		System.out.println("Size: " + set.size());
+	}
+}
